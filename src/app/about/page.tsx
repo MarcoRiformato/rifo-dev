01c80e57e510a2a8a57166592b9fb35b209e46e4
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2025-05-11 14:51:20 +0200

    Fix apostrophe formatting in the About page text for improved readability.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -83,10 +83,10 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-          Sono Marco Riformato. Elbano di nascita, risiedo sull&apos; isola, dove sviluppo il futuro.
+          Sono Marco Riformato. Elbano di nascita, risiedo sull&apos;isola, dove sviluppo il futuro.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
-          <p>Mi sono avvicinato al mondo digitale per curiosità e l'ho trasformato in mestiere con studio, pratica e costanza. Ho imparato a sviluppare da autodidatta, puntando da subito su un approccio pratico e orientato ai risultati.</p>
+          <p>Mi sono avvicinato al mondo digitale per curiosità e l&apos;ho trasformato in mestiere con studio, pratica e costanza. Ho imparato a sviluppare da autodidatta, puntando da subito su un approccio pratico e orientato ai risultati.</p>
 
           <p>Dal 2024 lavoro come freelance, portando online idee di business con metodo, velocità e attenzione ai dati. Oggi sviluppo siti web, piattaforme digitali ed esperienze online, aiutando aziende e professionisti a trasformare idee reali in progetti concreti.</p>
 
